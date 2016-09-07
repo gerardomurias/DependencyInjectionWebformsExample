ff29122d4b628e79135eaef9e499b9d3e2e00879
Gerardo <gerardomurias@hotmail.com>
2016-09-07 16:35:40 +0200

    first commit to github

---
+++ TestUnityWebForms/Clases/Dog.cs
@@ -0,0 +1,11 @@
+﻿namespace Clases
+{
+    public class Dog : IDog
+    {
+        public string Name { get; set; }
+        public void Bark()
+        {
+            Name = "guau";
+        }
+    }
+}
---
+++ TestUnityWebForms/Webforms/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+using Clases;
+using Microsoft.Practices.Unity;
+
+namespace Webforms
+{
+    public partial class _Default : Page
+    {
+        [Dependency]
+        public IDog Dog { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i = 1;
+
+        }
+    }
+}
